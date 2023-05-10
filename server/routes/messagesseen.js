1ff4b4270b3846cb230ca49b4c61151337ead062
kiralyjanos91 <kiralyjanos91@gmail.com>
2023-05-10 12:20:22 +0200

    message seen route no response bug fixed

--- server/routes/messagesseen.js
+++ server/routes/messagesseen.js
@@ -8,9 +8,17 @@ const messagesSeenRoute = ({
 
         const {userName , partnerName} = req.body
 
-        const user = await MemberModel.findOneAndUpdate({ username: userName , "private_messages.partner": partnerName} , {
-            $set: { "private_messages.$.unseen": 0 }
-        })
+        try {
+            const user = await MemberModel.findOneAndUpdate({ username: userName , "private_messages.partner": partnerName} , {
+                $set: { "private_messages.$.unseen": 0 }
+            })
+            res.status(200).json("seen")
+        }
+        catch(err) {
+            res.status(400).json(err)
+        }
+
+        
     })
 
     return router
